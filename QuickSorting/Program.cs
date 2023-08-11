d12bc88d7a04a1e228a98919e76efaec5cbca09d
Collapse29 <seryyzotov@gmail.com>
2023-08-11 16:20:48 +0500

    Добавьте файлы проекта.

---
+++ QuickSorting/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuickSorting
+{
+    internal class Program
+    {
+        static void HoareSort(int[] array, int start, int end)
+        {
+            if (end == start) return;
+            var pivot = array[end];
+            var storeIndex = start;
+            for (int i = start; i <= end - 1; i++)
+                if (array[i] <= pivot)
+                {
+                    var t = array[i];
+                    array[i] = array[storeIndex];
+                    array[storeIndex] = t;
+                    storeIndex++;
+                }
+            var n = array[storeIndex];
+            array[storeIndex] = array[end];
+            array[end] = n;
+            if (storeIndex > start) HoareSort(array, start, storeIndex - 1);
+            if (storeIndex < end) HoareSort(array, storeIndex + 1, end);
+        }
+
+        static void HoareSort(int[] array)
+        {
+            HoareSort(array, 0, array.Length -1);
+        }
+
+        static Random random = new Random();
+
+        static int[] GenerateArray(int length)
+        {
+            var array = new int[length];
+            for (int i = 0; i< array.Length; i++)
+                array[i]=random.Next();
+            return array;
+        }
+        static void Main(string[] args)
+        {
+          var array = GenerateArray(10);
+            HoareSort(array);
+            foreach (var e in array)
+                Console.WriteLine(e);
+        }
+    }
+}
